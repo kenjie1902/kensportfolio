f22256b29d59bef0c864469041e7165ca1325e13
kenjie1902 <kenneth.saniel@hcdc.edu.ph>
2025-06-13 04:20:20 +0800

    Rename files to PascalCase and fix import paths

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 'use client'
 import { useEffect, useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
-import HomeSection from './sections/homesection'
+import HomeSection from './sections/Homesection'
 import AboutSection from './sections/aboutsection'
 import ProjectsSection from './sections/projectssection'
 import CertificatesSection from './sections/certificatessection'
 import ContactSection from './sections/contactsection'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   const containerRef = useRef(null)
